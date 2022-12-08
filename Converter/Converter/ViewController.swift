b2316084dfe4abffaca9b5b0f5b05770a553227a
Roberto Guevara <robertoeguevara@gmail.com>
2022-12-08 10:10:42 -0800

    converter App

---
+++ Converter/Converter/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Converter
+//
+//  Created by Roberto on 2022-09-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tempText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func ConvertTemp(_ sender: Any) {
+        guard let tempString = tempText.text
+        else {
+            return
+        }
+        if let fahrenheit = Double (tempString){
+            let celsius = (fahrenheit - 32)/1.8
+            let resultText = "Celsius \(celsius)"
+            resultLabel.text = resultText
+        }
+    }
+}
+
