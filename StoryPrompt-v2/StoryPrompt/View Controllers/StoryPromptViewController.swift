871e8639a6da2b503b980af3e89a8fc9b48be6b0
Roberto Guevara <robertoeguevara@gmail.com>
2022-12-08 10:16:27 -0800

    storyPrompt app v2

---
+++ StoryPrompt-v2/StoryPrompt/View Controllers/StoryPromptViewController.swift
@@ -0,0 +1,28 @@
+//
+//  StoryPromptViewController.swift
+//  StoryPrompt
+//
+//  Created by laptop on 2022-11-01.
+//
+
+import UIKit
+
+class StoryPromptViewController: UIViewController {
+
+    @IBOutlet weak var storyPromptTextView: UITextView!
+    
+    var storyPrompt: StoryPromptEntry?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        storyPromptTextView.text = storyPrompt?.description
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+}
